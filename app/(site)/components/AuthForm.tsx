65dba0bb5bbb165f33eb3d377689ad8e5083ce63
hannah68 <reihaneh.naderi1989@gmail.com>
2023-05-25 10:54:45 +0100

    install react-toast and implement it

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,7 @@ import { Button } from "@/app/components/Button";
 import { AuthSocialButton } from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -33,7 +34,9 @@ export const AuthForm = () => {
 	const onSubmit: SubmitHandler<FieldValues> = (data) => {
 		setIsLoading(true);
 		if (variant === "REGISTER") {
-			axios.post('/api/register', data);
+			axios.post('/api/register', data)
+                .catch(() => toast.error('Something went wrong!'))
+                .finally(() => setIsLoading(false));
 		}
 		if (variant === "LOGIN") {
 			// NextAuth Signin
