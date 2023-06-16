18ec9ac67bd215de51c657f6ab60194bf58d9c22
hannah68 <reihaneh.naderi1989@gmail.com>
2023-06-16 12:02:09 +0100

    add online and offline

--- app/hooks/useOtherUser.ts
+++ app/hooks/useOtherUser.ts
@@ -7,12 +7,13 @@ const useOtherUser = (conversation: FullConversationType | {users: User[]}) => {
     const session = useSession();
 
     const otherUser = useMemo(() => {
-        const currentUserEmail = session?.data?.user?.email;
+        const currentUserEmail = session.data?.user?.email;
         const otherUser = conversation.users.filter((user) => user.email !== currentUserEmail);
-        return otherUser;
-    }, [session?.data?.user?.email, conversation.users]);
+        
+        return otherUser[0];
+    }, [session.data?.user?.email, conversation.users]);
 
-    return otherUser[0];
+    return otherUser;
 }
 
 export default useOtherUser;
